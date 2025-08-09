aad6e7dd0bca0b6deec83c153aa715568e9b7657
Your Name <you@example.com>
2025-08-08 20:37:28 -0400

    Implement traversal and insertion methods for Binary Search Tree

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -80,7 +80,13 @@ public class BinarySearchTree : IEnumerable<int>
 
     private void TraverseBackward(Node? node, List<int> values)
     {
-        // TODO Problem 3
+        // Problem 3: Traverse in reverse (right, node, left)
+        if (node is not null)
+        {
+            TraverseBackward(node.Right, values);
+            values.Add(node.Data);
+            TraverseBackward(node.Left, values);
+        }
     }
 
     /// <summary>
--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,19 +11,20 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
-        if (value < Data)
+        if (value == Data)
         {
-            // Insert to the left
+            // Do not insert duplicates
+            return;
+        }
+        else if (value < Data)
+        {
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else // value > Data
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -33,13 +34,20 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        // Problem 2: Search for value in BST
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Problem 4: Height of BST
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
